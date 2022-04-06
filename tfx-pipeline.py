9b3bf238df4e571ac40324e77337b7a3fa445937
MuhammedHatem <44292609+MuhammedHatem@users.noreply.github.com>
2022-04-06 15:43:25 +0200

    Update tfx-pipeline.py

--- tfx-pipeline.py
+++ tfx-pipeline.py
@@ -11,7 +11,7 @@ GOOGLE_CLOUD_PROJECT = 'dt-ml-pipeline'
 GOOGLE_CLOUD_PROJECT_NUMBER = '994103403822'  
 GOOGLE_CLOUD_REGION = 'us-central1'          
 GCS_BUCKET_NAME = 'dt-ml-pipeline-bucket' 
-
+_trainer_module_file = 'penguin_trainer.py'
 PIPELINE_NAME = 'ct-bigquery'
 
 # Path to various pipeline artifact.
